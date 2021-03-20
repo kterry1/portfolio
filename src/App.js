8515bd377281af9f811d476cabdf0d973ce5b4d7
Kevin <terrykevin1@gmail.com>
2021-03-20 11:40:47 -0400

    Added page transitions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import './App.css';
-import ParticlesBackground from './components/ParticlesBackground';
 import Home from './components/Home';
+import Projects from './components/Projects';
 import {Switch, Route} from 'react-router-dom';
+import { PageTransition } from "@steveeeie/react-page-transition";
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <ParticlesBackground/>
-    <Switch>
-     <Route path="/" component={Home}/>
-    </Switch>
-    </>
-  );
+    <Router>
+
+   <Route  render={({ location }) => {
+     console.log(location)
+     return (
+    <PageTransition
+      preset="fade"
+      exitAnimation="flipOutLeft"
+      enterAnimation="moveFromLeftFade"
+     transitionKey={location.pathname}
+    >
+       <Switch location={location}> 
+       <Route exact path="/" component={Home}/>
+       <Route exact path="/projects" component={Projects}/> 
+      </Switch> 
+     </PageTransition> 
+  )
+ }} 
+ />
+ </Router> 
+</>
+)
 }
 
 export default App;
