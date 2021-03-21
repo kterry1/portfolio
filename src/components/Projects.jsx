4d8baf381add2267a5a4e99b2d1a1940bf65f07e
Kevin <terrykevin1@gmail.com>
2021-03-21 16:46:28 -0400

    Added ball animation to Projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,75 +1,155 @@
-import React from 'react';
-import { Card, Avatar } from 'antd';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-
+import React from "react";
+import { Card, Avatar } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import tsImg from "../images/tsImg.jpg";
+import reactImg from "../images/reactImg.webp";
+import reduxImg from "../images/reduxImg.jpg";
 const Projects = () => {
-    const { Meta } = Card;
-    return (
-        <div className="projects_container">
-        <div className="projects">
-          <Card
-    style={{ width: 300 }}
-    cover={
-      <img
-        alt="example"
-        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-      />
-    }
-    actions={[
-      <SettingOutlined key="setting" />,
-      <EditOutlined key="edit" />,
-      <EllipsisOutlined key="ellipsis" />,
-    ]}
-  >
-    <Meta
-      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-      title="Drag and Drop"
-      description="TypeScript application that allows a user to drag and drop items on a Kibana Board."
-    />
-  </Card>
-          <Card
-    style={{ width: 300 }}
-    cover={
-      <img
-        alt="example"
-        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-      />
-    }
-    actions={[
-      <SettingOutlined key="setting" />,
-      <EditOutlined key="edit" />,
-      <EllipsisOutlined key="ellipsis" />,
-    ]}
-  >
-    <Meta
-      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-      title="Virtual Bank"
-      description="This is the description"
-    />
-  </Card>
-          <Card
-    style={{ width: 300 }}
-    cover={
-      <img
-        alt="example"
-        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-      />
-    }
-    actions={[
-      <SettingOutlined key="setting" />,
-      <EditOutlined key="edit" />,
-      <EllipsisOutlined key="ellipsis" />,
-    ]}
-  >
-    <Meta
-      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-      title="Coffee Cafe"
-      description="Logged in users can choose from a variety of cafe items and add them to their cart."
-    />
-  </Card>
-  </div>
-        </div>
-    )
-}
+  const { Meta } = Card;
+  return (
+    <div className="projects_container">
+      <h1>Hover over a card to see more</h1>
+      <h3>Click on a project to see the site</h3>
+
+      <div class="box">
+        <div class="ball"></div>
+        <div class="ball_two"></div>
+        <div class="ball_three"></div>
+      </div>
+      <div className="projects">
+        <Card
+          style={{
+            width: 300,
+            border: "1px solid #5f5f5f",
+            backgroundColor: "#242424",
+          }}
+          cover={
+            <img
+              style={{
+                minHeight: "200px",
+                backgroundColor: "#1d1d1d",
+                border: "1px solid #5f5f5f",
+              }}
+              // alt="example"
+              src={
+                "https://images.unsplash.com/photo-1530435460869-d13625c69bbf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              }
+            />
+          }
+        >
+          <Meta
+            avatar={<Avatar src={tsImg} />}
+            title="Drag and Drop"
+            description="TypeScript application that allows a user to drag and drop items on a Kibana Board."
+          />
+          <Meta
+            avatar={
+              <Avatar
+                src={reactImg}
+                style={{ position: "relative", bottom: "50px" }}
+              />
+            }
+          />
+          <Meta
+            avatar={
+              <Avatar
+                src={reduxImg}
+                style={{ position: "relative", bottom: "30px", right: "48px" }}
+              />
+            }
+          />
+        </Card>
+        <Card
+          style={{
+            width: 300,
+            border: "1px solid #5f5f5f",
+            backgroundColor: "#242424",
+          }}
+          cover={
+            <img
+              style={{
+                minHeight: "200px",
+                backgroundColor: "#1d1d1d",
+                border: "1px solid #5f5f5f",
+              }}
+              // alt="example"
+              src={
+                "https://images.unsplash.com/photo-1530435460869-d13625c69bbf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              }
+            />
+          }
+        >
+          <Meta
+            avatar={<Avatar src={tsImg} />}
+            title="Drag and Drop"
+            description="TypeScript application that allows a user to drag and drop items on a Kibana Board."
+          />
+          <Meta
+            avatar={
+              <Avatar
+                src={reactImg}
+                style={{ position: "relative", bottom: "50px" }}
+              />
+            }
+          />
+          <Meta
+            avatar={
+              <Avatar
+                src={reduxImg}
+                style={{ position: "relative", bottom: "30px", right: "48px" }}
+              />
+            }
+          />
+        </Card>
+        <Card
+          style={{
+            width: 300,
+            border: "1px solid #5f5f5f",
+            backgroundColor: "#242424",
+          }}
+          cover={
+            <img
+              style={{
+                minHeight: "200px",
+                backgroundColor: "#1d1d1d",
+                border: "1px solid #5f5f5f",
+              }}
+              // alt="example"
+              src={
+                "https://images.unsplash.com/photo-1530435460869-d13625c69bbf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              }
+            />
+          }
+        >
+          <Meta
+            avatar={<Avatar src={tsImg} />}
+            title="Drag and Drop"
+            description="TypeScript application that allows a user to drag and drop items on a Kibana Board."
+          />
+          <Meta
+            avatar={
+              <Avatar
+                src={reactImg}
+                style={{ position: "relative", bottom: "50px" }}
+              />
+            }
+          />
+          <Meta
+            avatar={
+              <Avatar
+                src={reduxImg}
+                style={{ position: "relative", bottom: "30px", right: "48px" }}
+              />
+            }
+          />
+        </Card>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
